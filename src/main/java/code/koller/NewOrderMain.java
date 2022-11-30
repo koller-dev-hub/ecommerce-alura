7b7f1247146b2d2fed9ba4ea32cf7d492faee015
William Koller <williamkoller404@gmail.com>
2022-11-30 20:51:25 -0300

    feat: add email and log service

--- src/main/java/code/koller/NewOrderMain.java
+++ src/main/java/code/koller/NewOrderMain.java
@@ -1,5 +1,6 @@
 package code.koller;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,13 +15,17 @@ public class NewOrderMain {
         var value = "1223,34445,1231231";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
 
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("success sent " +data.topic() + " :::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+            System.out.println("success sent " + data.topic() + " :::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        };
+        var email = "Welcome! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record,  callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
